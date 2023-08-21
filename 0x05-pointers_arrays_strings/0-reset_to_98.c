496820f0cff461734d2b5fea3b8946c3ad11e9dd
ay-sql <e.sqli.ayoub@gmail.com>
2023-08-21 06:14:59 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98.
+ * @n: A pointer to an integer.
+ *
+ * This function takes a pointer to an int as a parameter and updates
+ * the value it points to, setting it to 98.
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
